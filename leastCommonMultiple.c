d4a72bc9e5ede7d11aa18e363f978f14f1a979fe
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-14 16:17:54 +0000

    leastCommonMultiple start

---
+++ leastCommonMultiple.c
@@ -0,0 +1,19 @@
+//
+// Created by rodrigo on 2/14/23.
+//
+// Write a function in C that takes two positive integers as parameters and calculates and returns the
+//least common multiple of these two numbers.
+
+#include "stdio.h"
+
+int main(){
+    int n1, n2, lcm;
+    printf("Type the 2 numbers : \n");
+    scanf("%d;%d", &n1. &n2);
+    for (int i = 0; i < n1 ; ++i) {
+        if(n1 % i == 0){
+            
+        }
+    }
+
+}
