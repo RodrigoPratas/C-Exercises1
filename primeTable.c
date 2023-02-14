7edac7dd0173f35b9e636d2ac8334f17e297d4e6
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-14 23:30:47 +0000

    primetable start

---
+++ primeTable.c
@@ -0,0 +1,15 @@
+//
+// Created by rodrigo on 2/14/23.
+//
+//Write a function in C that generates a table of the first nth prime numbers, this being
+//value received in parameter and must present the table with 16 numbers in each line and all
+//right-aligned in columns.
+// 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53
+// 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131
+
+#include "stdio.h"
+
+int main(){
+
+
+}
