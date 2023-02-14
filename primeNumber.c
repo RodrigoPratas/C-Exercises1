9943af8abf3fe6cb69ddd37516c8b99c7ef661f9
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-14 20:41:02 +0000

    primen 1

--- primeNumber.c
+++ primeNumber.c
@@ -18,5 +18,13 @@ int main(){
         printf("Invalid number!!!");
         return 0;
     }
+    for(int i = 2; i < n; i++){
+        if(n % i == 0){
+        v = 0;
+        }
+        else{
+        v = 1;
+        }
+    }
 
 }
